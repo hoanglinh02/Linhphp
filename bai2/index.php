ca400f4e7e0007da29fe27b12caf6813ae449307
Your Namehoanglinh02 <linhhoangle1998@gmail.com>
2022-05-24 21:40:11 +0700

    first commit

---
+++ bai2/index.php
@@ -0,0 +1,43 @@
+<?php
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+</style>
+</head>
+<body>
+
+<h2>HTML Table</h2>
+
+<table>
+  <tr>
+    <th>ID</th>
+    <th>NAME</th>
+    <th>PRICE</th>
+  </tr>
+  <tr>
+    <td>Alfreds Futterkiste</td>
+    <td>Maria Anders</td>
+    <td>Germany</td>
+  </tr>
+ 
+
+</body>
+</html>
